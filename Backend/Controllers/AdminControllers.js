2282cb0d455954a9160bd6a5f83b58c58bb9ad1c
PAYUT.T <payuttobunma.pt@gmail.com>
2025-06-04 14:08:47 +0700

    update backend

--- Backend/Controllers/AdminControllers.js
+++ Backend/Controllers/AdminControllers.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import ImageKit from "imagekit";
 const imagekit = new ImageKit({
-  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  publicKey:  process.env.IMAGE_KIT_PUBLIC_KEY || 'public_pgZUE9kKgwJ6SoMX0HrBj45djnU=' ,
   privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT,
 });
