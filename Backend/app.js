26757f460bbe5ca03af759ca5782275b3abbd546
PAYUT.T <payuttobunma.pt@gmail.com>
2025-06-05 21:45:28 +0700

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -27,10 +27,11 @@ await fastify.register(fastifyMultipart, {
 });
 
 await fastify.register(cors, {
-  origin: ["https://schedule-pro-plum.vercel.app" , ["http://localhost:5173"]],
+  origin: ["https://schedule-pro-plum.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 });
+
 
 await fastify.register(rateLimit, {
   max: 100,
